070d21d653fd89da039e1056b062af89ffa5d099
Patrick Lu <lightning.lu7@gmail.com>
2019-04-02 10:58:43 -0700

    Update index.js

--- src/redux/index.js
+++ src/redux/index.js
@@ -5,8 +5,8 @@
 */
 
 import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
+import example from './example';
 
 export default combineReducers({
-  routing: routerReducer,
+  example,
 });
